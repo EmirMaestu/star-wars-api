3ce9a21a4fc83caae4b610990ecf6e172a2b8781
Emir Maestu <emir13_@hotmail.com>
2022-05-02 01:43:10 -0300

    add logo back to home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,22 @@
 import './Header.css'
-
+import { useHistory } from 'react-router-dom'
+import Logo from '../../img/logo.png'
 
 const Header = () => {
-  return (
-    <div>
-        <h1>SWAPI</h1>
-    </div>
-  )
+    let backToHome = useHistory()
+    const handleClick = () => {
+      backToHome.push('/')
+    }
+    return (
+        <div>
+            <img
+                onClick={() => handleClick()}
+                className='logo'
+                src={Logo}
+                alt='Logo SWAPI'
+            />
+        </div>
+    )
 }
 
 export default Header
